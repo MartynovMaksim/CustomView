b3581e999bf137763457873a99640c9aa22467c4
user <user>
2021-07-27 21:00:19 +0300

    don't work with view height = match_parent

--- app/src/main/java/com/example/customview/CustomView.kt
+++ app/src/main/java/com/example/customview/CustomView.kt
@@ -2,19 +2,31 @@ package com.example.customview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
-class CustomView(context: Context, attrs: AttributeSet): View(context,attrs) {
-    private val layoutHeight: Int
+class CustomView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private val multiply: Float
 
     init {
         val a = context.obtainStyledAttributes(attrs, R.styleable.CustomView)
-        val multiply = 0.156
-        layoutHeight = (a.getLayoutDimension(R.styleable.CustomView_layout_height,1500) * multiply).toInt()
+        multiply = a.getFloat(R.styleable.CustomView_multiply, 1.0F)
         a.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(widthMeasureSpec,layoutHeight)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+
+//        val height = (heightSize * multiply).toInt()
+//        val height: Int
+//
+//        if (heightMode == MeasureSpec.EXACTLY) {
+//            height = heightSize
+//        } else height = (heightSize * multiply).toInt()
+
+        val resolvedHeight = resolveSize((heightSize * multiply).toInt(),heightMeasureSpec)
+        setMeasuredDimension(widthSize, resolvedHeight)
     }
 }
